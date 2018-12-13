0697aee3f20385284daaeaab98634702414728d0
j.garcia.navarro <juanmanuel.gn@gmail.com>
2018-12-13 15:25:15 +0100

    [Option] Pattern matching

---
+++ src/main/scala/option/intermediate/OptionPatternMatching.scala
@@ -0,0 +1,20 @@
+package option.intermediate
+
+import option.OptionsToUse.{opEmpty, opSimpleInt}
+
+/**
+  * El método map nos permite aplicar una función al contenido de un Option.
+  * Resultados posibles
+  * Some(x).map(f) = Some(f(x))
+  * None.map(f) = None
+  */
+object OptionPatternMatching extends App {
+
+  println("Pattern matching with options")
+  val num = opSimpleInt match {
+    case Some(x) => x+3
+    case None => 0
+  }
+  println(s"Se aplica un pattern matching a un Option[Int] sumando 3 en el caso que sea Some(x) o devolviendo " +
+    s"0 en el caso que el option valga None: El resultado es = $num para Some(4)")
+}
