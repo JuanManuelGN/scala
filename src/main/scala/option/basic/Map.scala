8b36f3c6ab461a9a2e0a67304daa40d5dfd55d30
JuanManuelGN <juanmanuel.gn@gmail.com>
2020-02-13 12:26:40 +0100

    Improve comments

--- src/main/scala/option/basic/Map.scala
+++ src/main/scala/option/basic/Map.scala
@@ -11,7 +11,7 @@ import option.OptionsToUse.{optSimpleInt,optEmpty}
 object Map extends App {
 
   println("Option.map")
-  println(s"Se aplica la función sumar 8 al contenido del Option, devolviendo otro Option son la transformación: " +
+  println(s"Se aplica la función sumar 8 al contenido del Option, devolviendo otro Option con la transformación: " +
     s"Some(Map(a -> b)).map(_.+(8)) = ${optSimpleInt.map(_ + 8)}")
   println(s"Otra forma de hacer lo mismo que antes es: " +
     s"Some(Map(a -> b)).map(num => num + 8) = ${optSimpleInt.map(num => num + 8)}")
