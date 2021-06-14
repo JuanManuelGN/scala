0d961f786da618be4c1579abb176de020a037b9b
Juan Manuel García Navarro <juan.garcia@47deg.com>
2021-06-14 14:30:44 +0200

    training cats.Kleisi

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "0.1"
 
 scalaVersion := "2.12.7"
 
-libraryDependencies += "org.tukaani" % "xz" % "1.6"
-libraryDependencies += "org.apache.commons" % "commons-compress" % "1.18"
-libraryDependencies += "org.apache.ant" % "ant" % "1.10.5"
+libraryDependencies ++= Seq(
+  "org.tukaani" % "xz" % "1.6",
+  "org.apache.commons" % "commons-compress" % "1.18",
+  "org.apache.ant" % "ant" % "1.10.5",
+  "org.typelevel" %% "cats-core" % "2.0.0"
+)
---
+++ src/main/scala/catss/KleisiTraining.scala
@@ -0,0 +1,68 @@
+package catss
+
+import cats.data.Kleisli
+
+/**
+  * Entrenamiento con cats.Kleisi
+  */
+object KleisiTraining {
+
+  /**
+    * Tenemos dos funciones una que transforma un cadena a entero
+    * y otra que divide el 1.0 entre un entero pasado por parámetro
+    * las dos devuelven un Option[_]
+    * ¿Qué pasa si las intentamos compoener?
+    */
+  /**
+    * Transforma una cadena a entero
+    */
+  val parse: String => Option[Int] =
+    s => if (s.matches("-?[0-9]+")) Some(s.toInt) else None
+
+  /**
+    * Divide el número 1.0 entre el entero pasado como parámetro
+    */
+  val reciprocal: Int => Option[Double] =
+    i => if (i != 0) Some(1.0 / i) else None
+
+  /**
+    * La intentamos componer,
+    * como se puede observar si se descomenta la siguiente línea obtenemos un error de
+    * compilación siendo imposible componer estas funciones sin cambiar su signatura
+    * puesto que los tipos son incompatibles
+    */
+//  val reciParse = reciprocal.compose(parse)
+
+  /**
+    * Para resover esto podemos usar la clase Kleisi de cats
+    */
+
+  import cats.implicits._
+
+  val parseK: Kleisli[Option,String,Int] =
+    Kleisli((s: String) => if (s.matches("-?[0-9]+")) Some(s.toInt) else None)
+
+  val reciprocalK: Kleisli[Option,Int,Double] =
+    Kleisli((i: Int) => if (i != 0) Some(1.0 / i) else None)
+
+  val parseAndReciprocalK: Kleisli[Option,String,Double] =
+    reciprocalK.compose(parseK)
+}
+
+object KleisiTrainingRunner extends App {
+//  println(
+//    KleisiTraining.reciParse("4")
+//  )
+
+  println(
+    KleisiTraining.parse("4")
+  )
+
+  println(
+    KleisiTraining.reciprocal(4)
+  )
+
+  println(
+    KleisiTraining.parseAndReciprocalK("4")
+  )
+}
