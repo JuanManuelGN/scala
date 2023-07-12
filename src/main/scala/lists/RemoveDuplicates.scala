00676ab26ce13177569f4a29d4dcf279769b7acc
Juan Manuel García Navarro <juan.garcia@47deg.com>
2023-07-12 17:39:55 +0200

    add tail recursion version

--- src/main/scala/lists/RemoveDuplicates.scala
+++ src/main/scala/lists/RemoveDuplicates.scala
@@ -2,15 +2,17 @@ package lists
 
 import lists.model.{Item, Kind}
 
+import scala.annotation.tailrec
+
 class RemoveDuplicates {
 
   def rerank(kind: Kind, tail: List[Item]): List[Item] =
     tail match {
       case Nil => Nil
       case x :: Nil => List(x)
-      case xs => xs.find(_.kind == kind).fold{
+      case xs => xs.find(_.kind == kind).fold {
         xs
-      }{ item =>
+      } { item =>
         val (ys, zs) = xs.span(_ != item)
         item :: ys ::: (zs match {
           case Nil => List.empty[Item]
@@ -27,9 +29,9 @@ class RemoveDuplicates {
       case x :: Nil => List(x)
       case x :: xs if xs.exists(_.id == x.id) => {
         val itemToDelete = xs.find(_.id == x.id)
-        itemToDelete.fold{
+        itemToDelete.fold {
           x :: removeDuplicatesAndRerank(xs)
-        }{ item =>
+        } { item =>
           val (ys, zs) = xs.span(_ != item)
           (ys, zs) match {
             case (_, _ :: Nil) => x :: removeDuplicatesAndRerank(ys)
@@ -40,10 +42,32 @@ class RemoveDuplicates {
       case x :: xs => x :: removeDuplicatesAndRerank(xs)
     }
 
+  @tailrec
+  final def removeDuplicatesAndRerankTailRec(ls: List[Item], partialResult: List[Item] = Nil): List[Item] =
+    ls match {
+      case Nil => partialResult
+      case x :: Nil => partialResult :+ x
+      case x :: xs if xs.exists(_.id == x.id) => {
+        val itemToDelete = xs.find(_.id == x.id)
+        val (ys, zs) =
+          if (itemToDelete.isEmpty) {
+            (xs, partialResult :+ x)
+          } else {
+            val item = itemToDelete.get
+            val (yss, zss) = xs.span(_ != item)
+            (yss, zss) match {
+              case (_, _ :: Nil) => (yss, partialResult :+ x)
+              case (ysss, zsss) => (x :: ysss ++ rerank(zsss.head.kind, zsss.tail), partialResult)
+            }
+          }
+        removeDuplicatesAndRerankTailRec(ys, zs)
+      }
+      case x :: xs => removeDuplicatesAndRerankTailRec(xs, partialResult :+ x)
+    }
 
-//  def removeDuplicatesAndRerank(ls: List[Item]): List[Item] =
-//    ls.distinctBy(_.id)
-//  def removeDuplicatesAndRerank(ls: List[Item]): List[Item] =
-//    ls.distinct
 
+  //  def removeDuplicatesAndRerank(ls: List[Item]): List[Item] =
+  //    ls.distinctBy(_.id)
+  //  def removeDuplicatesAndRerank(ls: List[Item]): List[Item] =
+  //    ls.distinct
 }
