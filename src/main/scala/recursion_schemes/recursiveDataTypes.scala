6b27d2c916d2ae52daca9e487b829099bfeec74b
Juan Manuel García Navarro <juan.garcia@47deg.com>
2024-03-26 01:31:09 +0100

    add hylomorphin

--- src/main/scala/recursion_schemes/recursiveDataTypes.scala
+++ src/main/scala/recursion_schemes/recursiveDataTypes.scala
@@ -7,43 +7,43 @@ object recursiveDataTypes {
 
   sealed trait List
   case object Nil extends List
-  case class Cons(head: Int, tail: List) extends List
+  case class Cons(head: BigInt, tail: List) extends List
 
-  def multiply: List => Int = {
+  def multiply: List => BigInt = {
     case Nil => 1
     case Cons(i, t) => i * multiply(t)
   }
 
-  def length: List => Int = {
+  def length: List => BigInt = {
     case Nil => 0
     case Cons(i, t) => 1 + length(t)
   }
 
-  def foldList[A](onNil: A, onCons: (Int, A) => A): List => A = {
+  def foldList[A](onNil: A, onCons: (BigInt, A) => A): List => A = {
     case Nil => onNil
     case Cons(i, t) => onCons(i, foldList(onNil, onCons)(t))
   }
 
-  def multiplyFold: List => Int = foldList[Int](1, _ * _)
+  def multiplyFold: List => BigInt = foldList[BigInt](1, _ * _)
 
-  def lengthFold: List => Int = foldList[Int](0, (_, len) => len + 1)
+  def lengthFold: List => BigInt = foldList[BigInt](0, (_, len) => len + 1)
 
   sealed trait Tree
-  case class Leaf(value: Int) extends Tree
+  case class Leaf(value: BigInt) extends Tree
   case class Node(left: Tree, right: Tree) extends Tree
 
-  def foldTree[A](onLeaf: Int => A, onNode: (A, A) => A): Tree => A = {
+  def foldTree[A](onLeaf: BigInt => A, onNode: (A, A) => A): Tree => A = {
     case Leaf(i) => onLeaf(i)
     case Node(l, r) => onNode(foldTree(onLeaf, onNode)(l), foldTree(onLeaf, onNode)(r))
   }
 
-  def sum: Tree => Int = foldTree[Int](identity, _ + _)
+  def sum: Tree => BigInt = foldTree[BigInt](identity, _ + _)
 
-  def countLeaves: Tree => Int = foldTree[Int](_ => 1, _ + _)
+  def countLeaves: Tree => BigInt = foldTree[BigInt](_ => 1, _ + _)
 
   sealed trait ListF[A]
   case class NilF[A]() extends ListF[A]
-  case class ConsF[A](head: Int, a: A) extends ListF[A]
+  case class ConsF[A](head: BigInt, a: A) extends ListF[A]
 
   def in: ListF[List] => List = {
     case NilF() => Nil
@@ -73,7 +73,7 @@ object recursiveDataTypes {
     case ConsF(h, t) => Cons(h, t)
   }
 
-  def multiplyAlgebra: FAlgebra[ListF, Int] = {
+  def multiplyAlgebra: FAlgebra[ListF, BigInt] = {
     case NilF() => 1
     case ConsF(h, t) => h * t
   }
@@ -81,9 +81,9 @@ object recursiveDataTypes {
   def cata[F[_], R, A](algebra: F[A] => A, out: R => F[R])(r: R)(implicit F: Functor[F]): A =
     algebra(F.map(cata(algebra, out))(out(r)))
 
-  def multiplyList: List => Int = cata(multiplyAlgebra, out)
+  def multiplyList: List => BigInt = cata(multiplyAlgebra, out)
 
-  def range: Int => List = // ana
+  def range: BigInt => List = // ana
     n => if (n > 0) Cons(n, range(n - 1)) else Nil
 
   type Coalgebra[F[_], A] = A => F[A]
@@ -91,12 +91,12 @@ object recursiveDataTypes {
   def ana[F[_], R, A](coalgebra: Coalgebra[F, A], in: F[R] => R)(a: A)(implicit F: Functor[F]): R =
     in(F.map(ana(coalgebra, in))(coalgebra(a)))
 
-  def rangeCoalgebra: Coalgebra[ListF, Int] =
+  def rangeCoalgebra: Coalgebra[ListF, BigInt] =
     n => if (n > 0) ConsF(n, n - 1) else NilF()
 
-  def rangeF: Int => List = ana(rangeCoalgebra, inF)
+  def rangeF: BigInt => List = ana(rangeCoalgebra, inF)
 
-  def factorial: Int => Int = n => // hylomorphism
+  def factorial: BigInt => BigInt = n => // hylomorphism
     if (n > 0) n * factorial(n - 1) else 1
 
   def hylo[F[_], A, B](coalgebra: A => F[A], algebra: F[B] => B)(a: A)(implicit F: Functor[F]): B =
@@ -141,7 +141,7 @@ object Runner2 extends App {
   )
 
   println(
-    s"unfold and fold to calculate factorial using hylo ${factorialF(20)}"
+    s"unfold and fold to calculate factorial using hylo ${factorialF(100)}"
   )
 
 }
